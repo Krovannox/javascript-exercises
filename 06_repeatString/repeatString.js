d220d213ffccfd1bc7135faadd156f50f4d8875e
Krovannox <krovannox@proton.me>
2025-12-30 13:39:46 -0600

    Add solution to exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let newString = '';
 
+    if (times < 0) {
+        return console.log('ERROR');
+    } else {
+        for (let i = times; i > 0; i--) {
+        newString = newString + word;
+        }
+
+        return newString;
+    }
+
+    repeatString('hey', 3);
 };
 
 // Do not edit below this line
