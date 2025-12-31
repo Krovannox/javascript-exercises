58d22c80b7ed9db9ed514a125ba636c80f5ffb93
Krovannox <krovannox@proton.me>
2025-12-31 13:18:25 -0600

    Add solution for 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    return phrase.split('').reverse().join('');
 };
+
+reverseString('hello there'); // returns 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
