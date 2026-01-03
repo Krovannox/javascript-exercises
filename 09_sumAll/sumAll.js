2191c53760ce4f87f0daf625f7cd55793973ac17
Krovannox <krovannox@proton.me>
2026-01-03 12:19:06 -0600

    Add solution to exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(numberA, numberB) {
+    if (numberA < 0 || numberB < 0 || !Number.isInteger(numberA) || !Number.isInteger(numberB)) {
+        return 'ERROR';
+    }
+    
+    let start = Math.min(numberA, numberB);
+    let end = Math.max(numberA, numberB);
+    let sum = 0;
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
