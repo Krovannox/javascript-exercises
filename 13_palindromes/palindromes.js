4f68603ef203f5b63318e4cfea7e33865b79d6cc
Krovannox <krovannox@proton.me>
2026-01-28 05:10:27 -0600

    Add solution to exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let allowedCharacters = "abcdefghijklmnñopqrstuvwxyz1234567890";
 
+    let cleanString = string.toLowerCase()
+                            .split("")
+                            .filter(character => allowedCharacters.includes(character))
+                            .join("");
+
+    let reverseString = cleanString.split("")
+                                    .reverse()
+                                    .join("");
+
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
