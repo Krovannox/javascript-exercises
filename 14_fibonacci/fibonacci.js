c1d7f00eafd3c2325208999ad2bab11a81268c7d
Krovannox <krovannox@proton.me>
2026-01-28 05:11:48 -0600

    Add solution to exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let intNum = parseInt(num);
+    let prevA = 1;
+    let prevB = 0;
 
+    if (intNum === 0) return 0;
+    if (intNum < 0) return "OOPS";
+
+    for (let i = 2; i <= intNum; i++) {
+        let current = prevA + prevB;
+        prevB = prevA;
+        prevA = current;
+    }
+
+    return prevA;
 };
 
 // Do not edit below this line
