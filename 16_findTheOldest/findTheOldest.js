3851bbd04b7243da119f54fd545d10dcc15cd73b
Krovannox <krovannox@proton.me>
2026-01-29 05:03:24 -0600

    Add solution to exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+        const getYearsLived = function(birth, death) {
+          if (!death) {
+            death = new Date().getFullYear();
+          }
+          
+          return death - birth;
+        }
 
-};
+        return people.reduce((oldest, current) => {
+          let oldestAge = getYearsLived(oldest.yearOfBirth, oldest.yearOfDeath);
+          let currentAge = getYearsLived(current.yearOfBirth, current.yearOfDeath);
+          return oldestAge > currentAge ? oldest: current;
+        });
+      };
 
 // Do not edit below this line
 module.exports = findTheOldest;
