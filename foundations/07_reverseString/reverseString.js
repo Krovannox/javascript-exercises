92c06e49edb6fa7274453fc898b995704217c998
Krovannox <krovannox@proton.me>
2026-08-02 11:23:16 -0500

    Add initial solution to exercise-07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+    const stringArray = originalString.split("");
+    const stringArrayLength = stringArray.length - 1;
+    let reverseStringArray = [];
 
+    for (let i = stringArrayLength; i > -1; i--) {
+        reverseStringArray.push(stringArray[i]);
+    }
+
+    return reverseStringArray.join("");
 };
 
 // Do not edit below this line
